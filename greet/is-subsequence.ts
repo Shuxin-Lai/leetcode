6ab6a38d0daace830e0210403684629b226a05ad
Shuxin-Lai <shuxinlai001@gmail.com>
2023-01-03 22:18:20 +0800

    feat(greet): is subsequence

---
+++ greet/is-subsequence.ts
@@ -0,0 +1,17 @@
+function isSubsequence(s: string, t: string): boolean {
+  const len1 = s.length
+  const len2 = t.length
+  if (len1 > len2) {
+    return false
+  }
+
+  let i = 0
+  for (let j = 0; j < len2 && i < len1; j++) {
+    const char = t[j]
+    if (char == s[i]) {
+      i++
+    }
+  }
+
+  return i == len1
+}
